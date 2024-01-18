5cc5884d192b3a1dc1cbf5149d4e890f83ff14de
Drew Naumann <naumannadn@gmail.com>
2024-01-17 23:32:35 -0700

    this is really something

--- Mission2/Dice.cs
+++ Mission2/Dice.cs
@@ -9,5 +9,33 @@ namespace Mission2
     internal class Dice
     {
 
+        int numRolls = 0;
+        int[] rollsArray = new int[11];
+        Random rand = new Random();
+        
+        public Dice(int num) { 
+            numRolls = num;
+        }
+
+        public int[] rollDie()
+        {
+            for (int i = 0; i < numRolls; i++)
+            {
+
+                int DiceOne = rand.Next(1, 7);
+                int DiceTwo = rand.Next(1, 7);
+                int sumDice = DiceOne + DiceTwo;
+
+                rollsArray[sumDice - 2]++;
+            }
+
+            return rollsArray;
+        }
+
+        
+
+
+
+
     }
 }
--- Mission2/Program.cs
+++ Mission2/Program.cs
@@ -1,6 +1,9 @@
 ﻿// Author: Andrew Naumann
 // Section 001
+// Description: simulate rolling dice a bunch of times and display the percentages (represented
+// by stars) next to each possible number that can be rolled.
 
+using Mission2;
 using System;
 
 class Program
@@ -8,21 +11,46 @@ class Program
     static void Main()
     {
         int numRolls;
-        int[] rollsArray = new int[11];
-        Random rand = new Random();
+        //int[] rollsArray = new int[11];
+        float[] percentageArray = new float[11];
+        //Random rand = new Random();
 
         do
         {
-            // Prompt the user for input
-            Console.WriteLine("Enter an integer:");
+            // Present the user with the situation
+            Console.WriteLine("You are faced with two dice...\nEnter an integer (This is how many times you will roll the dice):");
 
-            // Read the user's input as a string
+            // Get input from the user
             string rollCount = Console.ReadLine();
 
-            // Try to convert the input to an integer
+            // If the user types an integer, start the program
+
             if (int.TryParse(rollCount, out numRolls))
             {
                 Console.WriteLine("You entered: " + numRolls);
+
+                Console.WriteLine("Rolling " + numRolls + " times...");
+
+                Dice Die = new Dice(numRolls);
+
+                int[] rolls = Die.rollDie();
+
+                for (int i = 0; i < rolls.Length; i++)
+                {
+                    percentageArray[i] = (rolls[i] * 100 / numRolls);
+                    int num = (int)percentageArray[i];
+
+                    Console.Write((i + 2) + ": ");
+
+                    for (int j = 0; j < num; j++)
+                    {
+                        Console.Write("*");
+                    }
+
+                    Console.WriteLine(" ");
+                }
+
+                Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
             }
             else
             {
@@ -30,23 +58,6 @@ class Program
             }
 
         } while (!IsInt(numRolls));
-
-        Console.WriteLine("Rolling " + numRolls + " times...");
-
-        for (int i = 0; i < numRolls; i++)
-        {
-            int DiceOne = rand.Next(1, 7);
-            int DiceTwo = rand.Next(1, 7);
-
-            int sumDice = DiceOne + DiceTwo;
-
-            rollsArray[sumDice - 2]++;
-        }
-
-        for (int i = 0; i < rollsArray.Length; i++ )
-        {
-            Console.WriteLine((i + 2) + ": " + rollsArray[i]);
-        }
     }
 
    
